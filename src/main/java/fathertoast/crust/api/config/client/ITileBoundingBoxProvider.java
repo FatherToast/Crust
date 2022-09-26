5947f11e0a514a323dff88cdbc2a93dd2842abb7
Sarinsa <mikkemusbruker@gmail.com>
2022-09-26 03:15:06 +0200

    Javadoc again

--- src/main/java/fathertoast/crust/api/config/client/ITileBoundingBoxProvider.java
+++ src/main/java/fathertoast/crust/api/config/client/ITileBoundingBoxProvider.java
@@ -6,13 +6,14 @@ import java.util.List;
 
 /**
  *  This interface can be implemented into a TileEntity
- *  that have bounding boxes that should be rendered
- *  (similar to how entity hitboxes are rendered).
+ *  that have bounding boxes that should be rendered in
+ *  debug mode (similar to how entity hitboxes are rendered).
  */
 public interface ITileBoundingBoxProvider {
 
     /**
-     * @return A List of bounding boxes that should be rendered in the world
+     * @return A List of bounding boxes that should be rendered in the world.
+     *         The provided bounding boxes will be rendered right before the TE.
      */
     List<AxisAlignedBB> getBoundingBoxes();
 }
