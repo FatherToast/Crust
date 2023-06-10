69dad5b7139c2d7dc56be4aab023465cf29f6fe5
FatherToast <father.toast@hotmail.com>
2023-06-10 10:52:15 -0500

    Fixed test impl

--- src/test/java/fathertoast/crust/test/common/TestCrust.java
+++ src/test/java/fathertoast/crust/test/common/TestCrust.java
@@ -4,7 +4,6 @@ import fathertoast.crust.api.CrustPlugin;
 import fathertoast.crust.api.ICrustApi;
 import fathertoast.crust.api.ICrustPlugin;
 import fathertoast.crust.api.config.common.ConfigManager;
-import fathertoast.crust.api.impl.CrustApi;
 import fathertoast.crust.common.core.Crust;
 import net.minecraft.util.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
