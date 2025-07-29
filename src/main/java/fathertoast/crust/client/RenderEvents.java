6714f881c01164e8cb45af9cd9d6a381b02def04
FatherToast <father.toast@hotmail.com>
2025-07-28 20:29:54 -0500

    Check if it's okay before looping instead of at each step

--- src/main/java/fathertoast/crust/client/RenderEvents.java
+++ src/main/java/fathertoast/crust/client/RenderEvents.java
@@ -17,13 +17,17 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import java.util.List;
 
 public class RenderEvents {
-
+    
     @SubscribeEvent
-    @SuppressWarnings("ConstantConditions")
+    @SuppressWarnings( "ConstantConditions" )
     public void onRenderLevelStage( RenderLevelStageEvent event ) {
         // Iterate through all block entities in proximity to the player
         // and check if they should render special bounding boxes
-        if ( ClientRegister.RENDER_SETTINGS.BLOCK_ENTITY_BB_RENDERING.enabled.get() && event.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES ) {
+        if( ClientRegister.RENDER_SETTINGS.BLOCK_ENTITY_BB_RENDERING.enabled.get() && event.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES ) {
+            // Don't render unless entity hitbox rendering is enabled
+            if( !Minecraft.getInstance().getEntityRenderDispatcher().shouldRenderHitBoxes() ||
+                    Minecraft.getInstance().showOnlyReducedInfo() ) return;
+            
             ClientLevel level = Minecraft.getInstance().level;
             BlockPos playerPos = Minecraft.getInstance().player.blockPosition();
             VertexConsumer linesBuffer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer( RenderType.lines() );
@@ -33,18 +37,18 @@ public class RenderEvents {
             int radius = Math.min(
                     ClientRegister.RENDER_SETTINGS.BLOCK_ENTITY_BB_RENDERING.distance.get(),
                     Minecraft.getInstance().options.getEffectiveRenderDistance() );
-
+            
             // Don't bother with a loop if we only render for one chunk
-            if ( radius <= 1 ) {
-                for ( BlockEntity blockEntity : level.getChunk( chunkX, chunkZ ).getBlockEntities().values() ) {
+            if( radius <= 1 ) {
+                for( BlockEntity blockEntity : level.getChunk( chunkX, chunkZ ).getBlockEntities().values() ) {
                     renderBoundingBoxes( blockEntity, event.getPoseStack(), event.getCamera().getPosition(), linesBuffer );
                 }
             }
             else {
-                for ( int x = chunkX - (radius - 1); x < chunkX + radius; x++ ) {
-                    for ( int z = chunkZ - (radius - 1); z < chunkZ + radius; z++ ) {
-                        if ( level.hasChunk( x, z ) ) {
-                            for ( BlockEntity blockEntity : level.getChunk( x, z ).getBlockEntities().values() ) {
+                for( int x = chunkX - (radius - 1); x < chunkX + radius; x++ ) {
+                    for( int z = chunkZ - (radius - 1); z < chunkZ + radius; z++ ) {
+                        if( level.hasChunk( x, z ) ) {
+                            for( BlockEntity blockEntity : level.getChunk( x, z ).getBlockEntities().values() ) {
                                 renderBoundingBoxes( blockEntity, event.getPoseStack(), event.getCamera().getPosition(), linesBuffer );
                             }
                         }
@@ -53,33 +57,28 @@ public class RenderEvents {
             }
         }
     }
-
-
+    
+    
     /**
      * Checks if the given BlockEntity is an instance of {@link IBlockEntityBBProvider}
      * and attempts to draw the bounding boxes it provides.
      */
     private void renderBoundingBoxes( BlockEntity blockEntity, PoseStack poseStack, Vec3 cameraPos, VertexConsumer buffer ) {
-        if ( blockEntity instanceof IBlockEntityBBProvider bbProvider ) {
+        if( blockEntity instanceof IBlockEntityBBProvider bbProvider ) {
             List<AABB> boxes = bbProvider.getBoundingBoxes();
-
-            if ( boxes == null || boxes.isEmpty() )
+            
+            if( boxes == null || boxes.isEmpty() )
                 return;
-
-            // Don't render unless entity hitbox rendering is enabled
-            if ( Minecraft.getInstance().getEntityRenderDispatcher().shouldRenderHitBoxes() &&
-                    !Minecraft.getInstance().showOnlyReducedInfo() ) {
-
-                poseStack.pushPose();
-                poseStack.translate( -cameraPos.x, -cameraPos.y, -cameraPos.z ); // Only move relative to camera position
-
-                for ( AABB box : boxes ) {
-                    if ( box != null ) {
-                        LevelRenderer.renderLineBox(poseStack, buffer, box, 0.0F, 1.0F, 0.0F, 1.0F);
-                    }
+            
+            poseStack.pushPose();
+            poseStack.translate( -cameraPos.x, -cameraPos.y, -cameraPos.z ); // Only move relative to camera position
+            
+            for( AABB box : boxes ) {
+                if( box != null ) {
+                    LevelRenderer.renderLineBox( poseStack, buffer, box, 0.0F, 1.0F, 0.0F, 1.0F );
                 }
-                poseStack.popPose();
             }
+            poseStack.popPose();
         }
     }
 }
