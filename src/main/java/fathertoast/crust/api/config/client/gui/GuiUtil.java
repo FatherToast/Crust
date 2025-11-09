251e7e8690ededdd3ebcf392f890b00ef2589505
Sarinsa <mikkemusbruker@gmail.com>
2025-11-09 16:04:46 +0100

    Javadoc stuff

--- src/main/java/fathertoast/crust/api/config/client/gui/GuiUtil.java
+++ src/main/java/fathertoast/crust/api/config/client/gui/GuiUtil.java
@@ -4,6 +4,8 @@ import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositione
 import org.joml.Vector2i;
 import org.joml.Vector2ic;
 
+import javax.annotation.Nonnull;
+
 public class GuiUtil {
     
     
@@ -15,42 +17,44 @@ public class GuiUtil {
          * putting them below the cursor.
          */
         public static final TooltipPositioner STANDARD = new TooltipPositioner( false );
-        /** Similar to {@link TooltipPositioner#STANDARD}, except tooltip gets centered on the X-axis. */
+        /**
+         * Similar to {@link TooltipPositioner#STANDARD}, except tooltip gets centered
+         * on the X-axis when possible without going off-screen.
+         */
         public static final TooltipPositioner CENTERED = new TooltipPositioner( true );
         
         
         /**
          * @param guiWidth      The width of the GUI
          * @param guiHeight     The height of the GUI
-         * @param x             The tooltip's X-position
-         * @param y             The tooltip's Y-position
+         * @param x             The tooltip's unmodified X-position
+         * @param y             The tooltip's unmodified Y-position
          * @param tooltipWidth  The width of the tooltip
          * @param tooltipHeight The height of the tooltip
-         * @return A vector containing the starting X and Y positions of the tooltip.
+         * @return A vector containing the new X and Y positions of the tooltip.
          */
         @Override
+        @Nonnull
         public Vector2ic positionTooltip( int guiWidth, int guiHeight, int x, int y, int tooltipWidth, int tooltipHeight ) {
-            Vector2i tooltipPos = (new Vector2i( x, y )).add( centered ? 0 : 12, -14 );
-            positionTooltip( guiWidth, tooltipPos, tooltipWidth );
-            return tooltipPos;
-        }
-        
-        private void positionTooltip( int guiWidth, Vector2i tooltipPos, int tooltipWidth ) {
+            Vector2i pos = (new Vector2i( x, y )).add( centered ? 0 : 12, -14 );
+            
             if( centered ) {
-                tooltipPos.x = tooltipPos.x - tooltipWidth / 2;
+                pos.x = pos.x - tooltipWidth / 2;
                 
-                if( tooltipPos.x < 4 )
-                    tooltipPos.x = 4;
-                else if( tooltipPos.x > (guiWidth - tooltipWidth) - 4 )
-                    tooltipPos.x = (guiWidth - tooltipWidth) - 4;
+                if( pos.x < 4 )
+                    pos.x = 4;
+                else if( pos.x > (guiWidth - tooltipWidth) - 4 )
+                    pos.x = (guiWidth - tooltipWidth) - 4;
             }
-            else if( tooltipPos.x + tooltipWidth > guiWidth ) {
-                tooltipPos.x = Math.max( tooltipPos.x - 24 - tooltipWidth, 4 );
+            else if( pos.x + tooltipWidth > guiWidth ) {
+                pos.x = Math.max( pos.x - 24 - tooltipWidth, 4 );
             }
             
-            if( tooltipPos.y < 10 ) {
-                tooltipPos.y = tooltipPos.y + 25;
+            if( pos.y < 10 ) {
+                pos.y = pos.y + 25;
             }
+            
+            return pos;
         }
     }
 }
