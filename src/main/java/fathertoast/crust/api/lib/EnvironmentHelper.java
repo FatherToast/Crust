54e19ab67ce290ca03ca60f5b10ced335a2f3c7a
Sarinsa <mikkemusbruker@gmail.com>
2026-03-15 19:24:24 +0100

    It sure do not be called "world" anymore

--- src/main/java/fathertoast/crust/api/lib/EnvironmentHelper.java
+++ src/main/java/fathertoast/crust/api/lib/EnvironmentHelper.java
@@ -8,14 +8,14 @@ public final class EnvironmentHelper {
     // ---- CHUNK METHODS ---- //
     
     /** @return True if the given position is in a loaded chunk. */
-    public static boolean isLoaded( LevelAccessor world, BlockPos pos ) { return isLoaded( world, pos.getX(), pos.getZ() ); }
+    public static boolean isLoaded( LevelAccessor level, BlockPos pos ) { return isLoaded( level, pos.getX(), pos.getZ() ); }
     
     /** @return True if the given position is in a loaded chunk. */
-    public static boolean isLoaded( LevelAccessor world, int x, int z ) { return isChunkPosLoaded( world, x >> 4, z >> 4 ); }
+    public static boolean isLoaded( LevelAccessor level, int x, int z ) { return isChunkPosLoaded( level, x >> 4, z >> 4 ); }
     
     /** @return True if the given chunk position is a loaded chunk. */
-    public static boolean isChunkPosLoaded( LevelAccessor world, int chunkX, int chunkZ ) {
-        return world.hasChunk( chunkX, chunkZ );
+    public static boolean isChunkPosLoaded( LevelAccessor level, int chunkX, int chunkZ ) {
+        return level.hasChunk( chunkX, chunkZ );
     }
     
     // Utility class
