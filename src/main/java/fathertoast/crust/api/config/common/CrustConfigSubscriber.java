50b8d772d3aa226d9fcf6d52564df708be91af40
Sarinsa <mikkemusbruker@gmail.com>
2026-04-19 22:37:24 +0200

    Whoops

--- src/main/java/fathertoast/crust/api/config/common/CrustConfigSubscriber.java
+++ src/main/java/fathertoast/crust/api/config/common/CrustConfigSubscriber.java
@@ -1,7 +1,5 @@
 package fathertoast.crust.api.config.common;
 
-import fathertoast.crust.api.ICrustPlugin;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.lang.annotation.ElementType;
@@ -9,14 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Marker annotation for classes that should be treated as plugins
- * for Crust. To make a plugin, simply decorate your plugin class with this
- * annotation. Crust will then attempt to find it at runtime, during
- * the {@link FMLCommonSetupEvent}.<br>
- * <br>
- * <strong>Note: your plugin class must also implement {@link ICrustPlugin}</strong>
- */
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.TYPE )
 @ApiStatus.Experimental
