e6e509f07a81a3a8f1bc7f57345b6be197159607
Sarinsa <mikkemusbruker@gmail.com>
2026-05-03 22:31:09 +0200

    More cleen

--- src/main/java/fathertoast/crust/api/config/common/field/AttributeListField.java
+++ src/main/java/fathertoast/crust/api/config/common/field/AttributeListField.java
@@ -21,7 +21,6 @@ import java.util.function.Predicate;
  *
  * @see ConfigDrivenAttributeModifierMap
  */
-
 public class AttributeListField extends GenericField<AttributeList> implements IStringListScreenEditable {
     
     /** Provides a detailed description of how to use attribute lists. Recommended to put at the top of any file using attribute lists. */
