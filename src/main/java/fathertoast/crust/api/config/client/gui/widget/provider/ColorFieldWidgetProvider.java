4d5ef54b1da924a3ca04c7dce46d760b3c30989d
Sarinsa <mikkemusbruker@gmail.com>
2026-05-04 02:00:51 +0200

    Comment out WIP code from ColorFieldWidgetProvider

--- src/main/java/fathertoast/crust/api/config/client/gui/widget/provider/ColorFieldWidgetProvider.java
+++ src/main/java/fathertoast/crust/api/config/client/gui/widget/provider/ColorFieldWidgetProvider.java
@@ -1,11 +1,15 @@
 package fathertoast.crust.api.config.client.gui.widget.provider;
 
+import fathertoast.crust.api.config.client.gui.screen.ColorPickerScreen;
+import fathertoast.crust.api.config.client.gui.screen.CrustConfigFileScreen;
 import fathertoast.crust.api.config.client.gui.widget.CrustConfigFieldList;
 import fathertoast.crust.api.config.client.gui.widget.field.ColorPreviewWidget;
 import fathertoast.crust.api.config.common.field.ColorIntField;
 import fathertoast.crust.api.config.common.file.TomlHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 import java.util.List;
@@ -34,13 +38,26 @@ public class ColorFieldWidgetProvider implements IConfigFieldWidgetProvider {
      */
     @Override
     public void apply( List<AbstractWidget> components, CrustConfigFieldList.FieldEntry listEntry, Object displayValue ) {
-        ColorPreviewWidget previewWidget = new ColorPreviewWidget( VALUE_WIDTH - ColorPreviewWidget.SIZE, 0 );
+        ColorPreviewWidget previewWidget = new ColorPreviewWidget( VALUE_WIDTH - ColorPreviewWidget.DEFAULT_SIZE, 0 );
         
         Number startValue = TomlHelper.asNumber( displayValue );
         previewWidget.setColor( startValue == null ? 0 : startValue.intValue(), FIELD.usesAlpha() );
         
+        // TODO - Add back once the color picker screen is done
+        /*
+        Button openPickerButton = new Button( VALUE_WIDTH - ColorPreviewWidget.DEFAULT_SIZE - 20, 0, 20, 20,
+                Component.literal( ":)" ), ( button ) -> openColorPickerScreen( listEntry ), Supplier::get );
+        
+        // noinspection resource
         EditBox editBox = new EditBox( listEntry.minecraft().font, 1, 1,
-                VALUE_WIDTH - 3 - ColorPreviewWidget.SIZE, VALUE_HEIGHT - 2, // Account for 1px frame
+                VALUE_WIDTH - 3 - ColorPreviewWidget.DEFAULT_SIZE - openPickerButton.getWidth(), VALUE_HEIGHT - 2, // Account for 1px frame
+                Component.literal( FIELD.getKey() ) );
+        editBox.setMaxLength( FIELD.getMinDigits() );
+         */
+        
+        // noinspection resource
+        EditBox editBox = new EditBox( listEntry.minecraft().font, 1, 1,
+                VALUE_WIDTH - 3 - ColorPreviewWidget.DEFAULT_SIZE, VALUE_HEIGHT - 2, // Account for 1px frame
                 Component.literal( FIELD.getKey() ) );
         editBox.setMaxLength( FIELD.getMinDigits() );
         
@@ -49,7 +66,7 @@ public class ColorFieldWidgetProvider implements IConfigFieldWidgetProvider {
         
         editBox.setResponder( ( value ) -> {
             Integer newValue = TomlHelper.parseHexInt( value );
-            if( newValue == null || !isValid( newValue ) ) {
+            if( newValue == null || !isValid( FIELD, newValue ) ) {
                 previewWidget.setColor( 0, true );
                 editBox.setTextColor( INVALID_COLOR );
                 listEntry.clearValue();
@@ -62,9 +79,21 @@ public class ColorFieldWidgetProvider implements IConfigFieldWidgetProvider {
         } );
         
         components.add( previewWidget );
+        //components.add( openPickerButton );
         components.add( editBox );
     }
     
+    // TODO - Use when the color picker screen is done
+    
+    /** Opens a color picker screen when the "open color picker" button is pressed. */
+    @SuppressWarnings( "unused" )
+    private void openColorPickerScreen( CrustConfigFieldList.FieldEntry listEntry ) {
+        if( Minecraft.getInstance().screen instanceof CrustConfigFileScreen screen ) {
+            Screen editScreen = new ColorPickerScreen( screen, listEntry, FIELD );
+            Minecraft.getInstance().setScreen( editScreen );
+        }
+    }
+    
     /** Returns true when the input number is valid. */
-    protected boolean isValid( Integer value ) { return FIELD.usesAlpha() || 0 <= value && value <= 0xFFFFFF; }
+    public static boolean isValid( ColorIntField field, Integer value ) { return field.usesAlpha() || 0 <= value && value <= 0xFFFFFF; }
 }
