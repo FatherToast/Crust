4033e3498f3e35d18e9888bfb4031f534c60f837
Sarinsa <mikkemusbruker@gmail.com>
2026-06-18 12:22:25 +0200

    Javadoc bink

--- src/main/java/fathertoast/crust/api/util/OnClient.java
+++ src/main/java/fathertoast/crust/api/util/OnClient.java
@@ -1,7 +1,8 @@
 package fathertoast.crust.api.util;
 
 /**
- * Purely decorative annotation used to mark classes/fields/methods as only being referenced on client
- * even if they are outside an expected client package.
+ * Purely decorative annotation used to mark classes/fields/methods as being
+ * <strong>client-side</strong> only, as in that said members are not meant to be interacted
+ * with outside the client game distribution.
  */
 public @interface OnClient { }
