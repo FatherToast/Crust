dec63ff0fbcc48488f50c02e73c1057261f7f701
Sarinsa <mikkemusbruker@gmail.com>
2026-07-30 16:37:00 +0200

    Added JEI compat to stop JEI from rendering bookmarks over Crust's extra inv buttons

--- src/test/java/fathertoast/crust/test/client/TestClientForgeEventHandler.java
+++ src/test/java/fathertoast/crust/test/client/TestClientForgeEventHandler.java
@@ -6,6 +6,7 @@ import fathertoast.crust.api.ICrustApi;
 import fathertoast.crust.api.client.SortedKeyMapping;
 import fathertoast.crust.api.config.common.field.EnvironmentListField;
 import fathertoast.crust.client.ClientRegister;
+import fathertoast.crust.client.ScreenEvents;
 import fathertoast.crust.client.config.CfgEditorCrustConfig;
 import fathertoast.crust.client.screen.widget.button.ExtraMenuButton;
 import fathertoast.crust.test.common.TestCrust;
@@ -14,6 +15,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
@@ -117,6 +120,29 @@ public class TestClientForgeEventHandler {
                     Component.literal( "Open test screen" ),
                     button -> screen.getMinecraft().setScreen( new TestScreen( screen ) ),
                     Supplier::get ) );
+        }
+    }
+    
+    /** Called right before a screen is being rendered. */
+    @SubscribeEvent
+    public static void onRenderScreen( ScreenEvent.Render.Pre event ) {
+        if( !ClientRegister.EXTRA_INV_BUTTONS.GENERAL.enabled.get() )
+            return;
+        
+        // Render a translucent rectangle behind the extra inventory buttons
+        // to properly visualize X, Y, width and height of the occupied area
+        if( event.getScreen() instanceof AbstractContainerScreen ) {
+            if( ScreenEvents.INV_BUTTONS_AREA != null ) {
+                final Rect2i rect = ScreenEvents.INV_BUTTONS_AREA;
+                // noinspection ConstantConditions
+                event.getGuiGraphics().fill(
+                        rect.getX(),
+                        rect.getY(),
+                        rect.getWidth() + rect.getX(),
+                        rect.getHeight() + rect.getY(),
+                        0xAAEE2222
+                );
+            }
         }
     }
 }
