b1db26ee94f58856c00177b56f66fe988d647d39
Sarinsa <mikkemusbruker@gmail.com>
2026-08-13 20:44:37 +0200

    Fix hotswap explotando

--- src/test/java/fathertoast/crust/test/common/TestGameEventHandler.java
+++ src/test/java/fathertoast/crust/test/common/TestGameEventHandler.java
@@ -9,12 +9,11 @@ import fathertoast.crust.api.event.advancement.IModifiableReward;
 import net.minecraft.advancements.FrameType;
 import net.minecraft.advancements.RequirementsStrategy;
 import net.minecraft.advancements.critereon.ItemUsedOnLocationTrigger;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.FireworkRocketEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
@@ -31,7 +30,7 @@ public class TestGameEventHandler {
     private static int elCountastico;
     
     //@SubscribeEvent // Comment the annotation out to 'turn off' this console spam
-    static void onServerTick( TickEvent.ServerTickEvent event ) {
+    public static void onServerTick( TickEvent.ServerTickEvent event ) {
         if( event.phase == TickEvent.Phase.END ) {
             if( elCountastico++ > 40 ) {
                 elCountastico = 0;
@@ -49,7 +48,7 @@ public class TestGameEventHandler {
     }
     
     @SubscribeEvent( priority = EventPriority.NORMAL )
-    static void onLivingHurt( LivingHurtEvent event ) {
+    public static void onLivingHurt( LivingHurtEvent event ) {
         final Level level = event.getEntity().level();
         
         if( level.isClientSide() ) return;
@@ -73,19 +72,16 @@ public class TestGameEventHandler {
                 TestCrust.LOG.debug( "Values = {} for entity: {}", doubles, entityType );
             }
             
-            // Test number set
-            ItemStack heldStack = player.getMainHandItem();
-            
-            if( TestCrust.CONFIG.GENERAL.numberSetField.contains( heldStack.getCount() ) ) {
-                // Hurrah! Let us celebrate with murder and fireworks :D
-                FireworkRocketEntity rocket = new FireworkRocketEntity( level, new ItemStack( Items.FIREWORK_ROCKET ), event.getEntity() );
-                level.addFreshEntity( rocket );
+            // Test number list
+            for( float f : TestCrust.CONFIG.GENERAL.numberListField.entries() ) {
+                player.displayClientMessage( Component.literal( "Next float: " + f ), false );
             }
+            player.displayClientMessage( Component.literal( "---------------------------" ), false );
         }
     }
     
     @SubscribeEvent( priority = EventPriority.NORMAL )
-    static void onAdvancementLoad( AdvancementLoadEvent event ) {
+    public static void onAdvancementLoad( AdvancementLoadEvent event ) {
         final ResourceLocation advancementId = event.getId();
         
         /// Test for modifying criteria.
